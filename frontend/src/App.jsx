d7198691daa20641887593b4379b2748f9e9d3df
Chetan Patil <patilchetan946@gmail.com>
2025-07-08 21:09:27 +0530

    Frontend Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import toast from "react-hot-toast";
+import JoinCreateChat from "./components/JoinCreateChat";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      Home
-      <button onClick={() => toast.success("This is toast")}>Clickme</button>
+      <JoinCreateChat />
     </div>
   );
 }
